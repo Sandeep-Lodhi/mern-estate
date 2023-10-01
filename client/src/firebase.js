de968301471a4d9e84aabe4ffc264f423abfe59c
Sandeep-Lodhi <officialsandeeplodhi@gmail.com>
2023-10-01 19:25:30 +0530

    install react js and tailwind css and create the first template

---
+++ client/src/firebase.js
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-456e0.firebaseapp.com",
+  projectId: "mern-estate-456e0",
+  storageBucket: "mern-estate-456e0.appspot.com",
+  messagingSenderId: "393982264619",
+  appId: "1:393982264619:web:3cd13f4ec61b730be1b16e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+
+// -------------------------------------------------------------
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+
+
